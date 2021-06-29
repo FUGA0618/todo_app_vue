e70840b659fc8c9f0c53e5f85268552142781a0a
FUGA0618 <aax.chiri@gmail.com>
2021-06-29 21:16:37 +0900

    Deleteアイコンを追加

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <i class="bi bi-trash-fill"></i>
+</template>
+
+<script>
+export default {
+  name: 'DeleteButton',
+}
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,21 @@
 <template>
   <ul class="list-group">
-    <li class="list-group-item" v-for="(value, key) in todoItems"
-                                v-bind:key="key">{{ value }}</li>
+    <li class="list-group-item"
+        v-for="(value, key) in todoItems"
+        v-bind:key="key">{{ value }}
+      <DeleteButton />
+    </li>
   </ul>
 </template>
 
 <script>
+import DeleteButton from "./DeleteButton.vue"
+
 export default {
   name: 'TodoList',
   props: ['todoItems'],
+  components: {
+    DeleteButton
+  }
 }
 </script>
