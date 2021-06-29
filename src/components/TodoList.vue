09ffe99ba6e0f548b99bfc4e2dd8aaef6b30bdfc
FUGA0618 <aax.chiri@gmail.com>
2021-06-29 15:11:59 +0900

    TodoListコンポーネントを作成

---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Enter the contents of the to-do list." aria-label="Enter the contents of the to-do list." aria-describedby="button-addon2">
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Add ToDo</button>
+  </div>
+
+  <div>
+    <ul class="list-group">
+      <li class="list-group-item">TodoTodoTodo</li>
+      <li class="list-group-item">TodoTodoTodo</li>
+      <li class="list-group-item">TodoTodoTodo</li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'TodoList'
+}
+</script>
